8c2760a84f76636395ce20c924a48c64ed7e3292
Nathaniel Richards <nathaniel@datacompusa.com>
2021-06-11 02:48:30 -0400

    tried to do race lang select

--- src/pages/App/Character/NewCharacter/Race.tsx
+++ src/pages/App/Character/NewCharacter/Race.tsx
@@ -1,6 +1,8 @@
 import { ChevronRightIcon } from '@heroicons/react/solid';
 import axios from 'axios';
 import { useState } from 'react';
+import { useHistory } from 'react-router';
+import phb from '../../../../core/phb';
 import reference from '../../../../core/reference';
 import { Race as RaceType } from '../../../../core/types';
 
@@ -8,13 +10,21 @@ const BASE_URL = 'https://api.open5e.com/races/?search=';
 
 const Race = ({
 	race,
-	keyVal
+	keyVal,
+	setSelectedRace,
+	selectedRace,
+	individual = false
 }: {
 	race: RaceType;
 	keyVal: number;
 	setSelectedRace: React.Dispatch<React.SetStateAction<RaceType | undefined>>;
 	selectedRace: RaceType | undefined;
+	individual?: boolean;
 }) => {
+	const history = useHistory();
+
+	const [languagesSelectedLength, setLanguagesSelectedLength] = useState(0);
+
 	const [open, setOpen] = useState(false);
 	const [loading, setLoading] = useState(true);
 	const [raceData, setRaceData] = useState<any>(undefined);
@@ -49,10 +59,14 @@ const Race = ({
 									sub.name === 'Lightfoot' ||
 									sub.name === 'Stout'
 							);
+					} else if (race.text.includes('Gnome')) {
+						raceRes = res.data.results
+							.find((result: any) => result.name === 'Gnome')
+							.subraces.find(
+								(sub: any) => sub.name === race.text
+							);
 					}
 				}
-
-				console.log(raceRes);
 
 				if (raceRes) {
 					const desc = getDesc(raceRes.desc);
@@ -76,21 +90,23 @@ const Race = ({
 
 	return (
 		<div
-			className="race flex flex-col shadow-md bg-white mb-5 px-4 pt-3 pb-3"
+			className="race flex flex-col shadow-md bg-white mb-5 px-4 pt-3 pb-3 cursor-pointer"
 			key={keyVal}
+			onClick={() => {
+				if (!raceData && !individual) {
+					getRaceData();
+					setOpen(!open);
+				}
+				if (individual) {
+					history.push('/app/characters/new/classes');
+				}
+			}}
 		>
 			<div className="top flex justify-between items-center">
 				<h4>{race.text}</h4>
-				<div className="  has-tooltip cursor-pointer flex rounded-md">
-					<ChevronRightIcon
-						className="h-6 w-6 "
-						onClick={() => {
-							if (!raceData) {
-								getRaceData();
-							}
-							setOpen(!open);
-						}}
-					/>
+
+				<div className="has-tooltip cursor-pointer flex rounded-md">
+					{!individual && <ChevronRightIcon className="h-6 w-6 " />}
 				</div>
 			</div>
 			{open && (
@@ -103,76 +119,193 @@ const Race = ({
 										{race.text}
 									</h3>
 								</div>
-								{!loading ? (
-									<div className="relative p-6 flex-auto">
-										<p>
-											{!raceData &&
-												'Content not yet available for this race'}
-										</p>
-										{raceData && (
-											<div className="">
-												<p className="font-bold my-2">
-													Description
-												</p>
-												<p className="my-4 text-gray-500 text-md leading-relaxed">
-													{raceData.desc}
-												</p>
-												<p className="font-bold my-2">
-													Speed
-												</p>
-												<p className="my-4 text-gray-500 text-md leading-relaxed">
-													{race.speed} feet
-												</p>
-												<p className="font-bold my-2">
-													Size
-												</p>
-												{raceData.size && (
-													<p className="my-4 text-gray-500 text-md leading-relaxed">
-														{raceData.size.replace(
-															'**_Size._**',
-															''
-														)}
+								<div
+									style={{ maxHeight: '80vh' }}
+									className="overflow-y-scroll"
+								>
+									{!loading ? (
+										<div className="relative p-6 flex-auto">
+											<p>
+												{!raceData &&
+													'Content not yet available for this race'}
+											</p>
+											{raceData && (
+												<div className="">
+													<p className="font-bold my-2">
+														Description
 													</p>
-												)}
-												<p className="font-bold my-2">
-													Ability Score Increase
-												</p>
-												{raceData.asi.map(
-													(asi: any, key: number) => (
-														<p
-															key={key}
-															className="my-4 text-gray-500 text-md leading-relaxed"
-														>
-															- {asi.attributes} +
-															{asi.value}
+													<p className="my-4 text-gray-500 text-md leading-relaxed">
+														{raceData.desc}
+													</p>
+													<p className="font-bold my-2">
+														Speed
+													</p>
+													<p className="my-4 text-gray-500 text-md leading-relaxed">
+														{race.speed} feet
+													</p>
+
+													{raceData.size && (
+														<>
+															<p className="font-bold my-2">
+																Size
+															</p>
+															<p className="my-4 text-gray-500 text-md leading-relaxed">
+																{raceData.size.replace(
+																	'**_Size._**',
+																	''
+																)}
+															</p>
+														</>
+													)}
+													<p className="font-bold my-2">
+														Ability Score Increase
+													</p>
+													{raceData.asi.map(
+														(
+															asi: any,
+															key: number
+														) => (
+															<p
+																key={key}
+																className="my-4 text-gray-500 text-md leading-relaxed"
+															>
+																-{' '}
+																{asi.attributes}{' '}
+																+{asi.value}
+															</p>
+														)
+													)}
+													<p className="font-bold my-2">
+														Extra Language Selection
+														Choose (
+														{race.extraLanguages})
+													</p>
+
+													{race.extraLanguages > 0 ? (
+														<>
+															{reference.languages.map(
+																(lang, key) => (
+																	<div
+																		key={
+																			key
+																		}
+																	>
+																		<input
+																			onChange={e => {
+																				if (
+																					race.languages.includes(
+																						Number(
+																							e
+																								.target
+																								.value
+																						) as any
+																					)
+																				) {
+																					const index =
+																						race.languages.indexOf(
+																							Number(
+																								e
+																									.target
+																									.value
+																							) as any
+																						);
+																					race.languages.splice(
+																						index,
+																						1
+																					);
+																					setLanguagesSelectedLength(
+																						languagesSelectedLength -
+																							1
+																					);
+																				} else {
+																					if (
+																						!race.languages.includes(
+																							lang.id
+																						)
+																					) {
+																						race.languages.push(
+																							Number(
+																								e
+																									.target
+																									.value
+																							) as any
+																						);
+																						setLanguagesSelectedLength(
+																							languagesSelectedLength +
+																								1
+																						);
+																					}
+																				}
+																			}}
+																			value={
+																				lang.id
+																			}
+																			checked={race.languages.includes(
+																				lang.id
+																			)}
+																			type="checkbox"
+																		/>
+																		<p>
+																			{
+																				lang.text
+																			}
+																		</p>
+																	</div>
+																)
+															)}
+														</>
+													) : (
+														<p className="my-4 text-gray-500 text-md leading-relaxed">
+															No extra languages
+															for this race.
 														</p>
-													)
-												)}
-												<p className="font-bold my-2">
-													Languages
-												</p>
-												{race.languages.map(
-													(language, key) => (
-														<p
-															key={key}
-															className="my-4 text-gray-500 text-md leading-relaxed"
-														>
-															-{' '}
-															{
-																reference
-																	.languages[
-																	language
-																].text
-															}
+													)}
+													<p className="font-bold my-2">
+														Languages
+													</p>
+													{race.languages.map(
+														(language, key) => (
+															<p
+																key={key}
+																className="my-4 text-gray-500 text-md leading-relaxed"
+															>
+																-{' '}
+																{
+																	reference
+																		.languages[
+																		language
+																	].text
+																}
+															</p>
+														)
+													)}
+													<p className="font-bold my-2">
+														Extra Features
+													</p>
+													{race.extraFeatures.map(
+														(f, key) => (
+															<p
+																key={key}
+																className="my-4 text-gray-500 text-md leading-relaxed"
+															>
+																- {f.text}
+															</p>
+														)
+													)}
+													{race.extraFeatures.length <
+														1 && (
+														<p className="my-4 text-gray-500 text-md leading-relaxed">
+															No extra features
+															for this race.
 														</p>
-													)
-												)}
-											</div>
-										)}
-									</div>
-								) : (
-									<div className="mt-5 mb-5 mx-auto loader ease-linear rounded-full border-4 border-t-4 border-gray-200 w-16 h-16 "></div>
-								)}
+													)}
+												</div>
+											)}
+										</div>
+									) : (
+										<div className="mt-5 mb-5 mx-auto loader ease-linear rounded-full border-4 border-t-4 border-gray-200 w-16 h-16 "></div>
+									)}
+								</div>
 								<div className="flex items-center justify-between p-3 border-t border-solid border-blueGray-200 rounded-b">
 									<button
 										className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -184,9 +317,15 @@ const Race = ({
 									<button
 										className="text-red-500 background-transparent  font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 										type="button"
-										onClick={() => setOpen(false)}
+										onClick={() => {
+											setSelectedRace(race);
+											setOpen(false);
+										}}
 									>
-										Confirm
+										{selectedRace?.text === race.text ||
+										individual
+											? 'Continue'
+											: 'Confirm'}
 									</button>
 								</div>
 							</div>
@@ -194,6 +333,11 @@ const Race = ({
 					</div>
 					<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
 				</>
+			)}
+			{individual && (
+				<button className="bg-red-500 w-50 h-auto mb-4 hover:bg-red-500 text-white font-bold py-2 px-4 mt-5 rounded">
+					Continue
+				</button>
 			)}
 		</div>
 	);
--- src/pages/App/Character/NewCharacter/Races.tsx
+++ src/pages/App/Character/NewCharacter/Races.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { Redirect } from 'react-router';
+import { useEffect, useState } from 'react';
+import { Redirect, useHistory } from 'react-router';
 import { Animate } from 'react-simple-animate';
 import phb from '../../../../core/phb';
 import { Race as RaceType } from '../../../../core/types';
+import { CharacterActions } from '../../../../redux/Character/actions';
 import { CharacterSelectors } from '../../../../redux/Character/selectors';
 import { animProps } from '../../../Onboarding/Login';
 import Race from './Race';
@@ -10,10 +11,24 @@ import Race from './Race';
 const races = phb.races;
 
 const Races = () => {
+	const history = useHistory();
 	const newCharacter = CharacterSelectors.useSelectNewCharacter();
+	const updateCharacter = CharacterActions.useUpdateNewCharacter();
 
 	const [selectedRace, setSelectedRace] =
 		useState<RaceType | undefined>(undefined);
+
+	useEffect(() => {
+		if (selectedRace) {
+			const newChar = newCharacter;
+
+			if (newChar) {
+				newChar.race = selectedRace;
+				updateCharacter(newChar);
+				history.push('/app/characters/new/classes');
+			}
+		}
+	}, [newCharacter, history, selectedRace, updateCharacter]);
 
 	if (!newCharacter || newCharacter.playerName.length < 1) {
 		return <Redirect to="/app/characters/new" />;
@@ -25,6 +40,16 @@ const Races = () => {
 				<h3 className="text-lg mb-3 font-bold">Select a race</h3>
 				<hr />
 				<div className="races mt-8 text-black pb-5 mx-auto">
+					{newCharacter.race && (
+						<Race
+							individual={true}
+							keyVal={-1}
+							setSelectedRace={setSelectedRace}
+							selectedRace={selectedRace}
+							race={newCharacter.race}
+						/>
+					)}
+
 					{races.map((race, key) => (
 						<Race
 							key={key}
