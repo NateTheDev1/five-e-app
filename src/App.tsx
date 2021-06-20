4a6cd1623f1234b495a94f4abb9f2789b783207d
Nathaniel Richards <nathaniel@datacompusa.com>
2021-06-20 02:54:11 -0400

    Added swipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
+
 import BaseSchema from './components/BaseSchema';
 import Loading from './components/Loading';
 import ErrorBoundary from './ErrorBoundary';
@@ -11,11 +12,6 @@ import Router from './Router';
 // LogRocket.init('5e-sidekick/5e-sidekick');
 
 const App = () => {
-	localStorage.setItem(
-		'fivetoken',
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsInVzZXJuYW1lIjoibnRobmxyaWNoYXJkc0BnbWFpbC5jb20iLCJpYXQiOjE2MjM2OTU1NTksImV4cCI6MTYyNjI4NzU1OX0.fOapOcDvbx6soQ0EeIyfJPquTYRu8gx8BbeTUZIWR9Y'
-	);
-
 	return (
 		<ErrorBoundary>
 			<Provider store={store}>
