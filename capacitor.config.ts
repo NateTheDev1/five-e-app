f15c1b431b7d1dc66bdcf7f0315beb2bca21cff4
Nathaniel Richards <nathaniel@datacompusa.com>
2021-06-21 01:57:58 -0400

    Removed server

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,11 @@ const config: CapacitorConfig = {
 	appId: 'com.dnd.sidekick',
 	appName: '5E Sidekick',
 	webDir: 'build',
-	bundledWebRuntime: false,
-	server: {
-		url: 'http://10.0.0.147:3000',
-		cleartext: true
-	}
+	bundledWebRuntime: false
+	// server: {
+	// 	url: 'http://10.0.0.147:3000',
+	// 	cleartext: true
+	// }
 };
 
 export default config;
